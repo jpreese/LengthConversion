b5c3e3f5ba1a9a8f7071e5d0f4d2e1f8972de6c2
John Reese <jpreese@mtu.edu>
2016-03-26 01:17:48 -0400

    initial commit

---
+++ LengthConversion/Decorations/UnitDecorator.cs
@@ -0,0 +1,20 @@
+﻿using LengthConversion.Models;
+
+namespace LengthConversion.Decorations
+{
+    class UnitDecorator : Decorator
+    {
+        private readonly Unit _unit;
+
+        public UnitDecorator(Result result, Unit unit) : base (result)
+        {
+            _unit = unit;
+            Value = result.Value;
+        }
+
+        public override string GetValue()
+        {
+            return string.Join(" ", _result.GetValue(), _unit);
+        }
+    }
+}
